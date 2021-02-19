1efa8c03ad2fd9e0a1b4e49e3f624688123d5561
Andrew Bonas <aabonas@hotmail.com>
2021-02-19 13:35:36 -0800

    initial version

---
+++ src/components/Overview.js
@@ -0,0 +1,23 @@
+import React from "react";
+import uniqid from "uniqid";
+
+const Overview = (props) => {
+  const {
+    tasks
+  } = props;
+
+  return ( <
+    ul className = "task-list" > {
+      tasks.map((task) => {
+        return <li key = {
+          uniqid()
+        } > {
+          task
+        } < /li>;
+      })
+    } <
+    /ul>
+  );
+};
+
+export default Overview;
